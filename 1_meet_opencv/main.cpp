32f7f50bba7d98956b25acfb18bb6085aee88546
win7_64_en <1281261856@qq.com>
2017-04-23 14:32:59 +0800

    add opencv310All.props succeed

--- 1_meet_opencv/main.cpp
+++ 1_meet_opencv/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
-
+#include <opencv2/opencv.hpp>
 
 using namespace std;
-
+using namespace cv;
 
 int main()
 {
 	cout << "Hello OpenCV!" << endl;
+
+	Mat img = imread("pp.jpg");
+	if (img.empty()) {
+		cout << "error";
+		return -1;
+	}
+	imshow("ppµÄö¦ÕÕ", img);
+	waitKey();
 }
