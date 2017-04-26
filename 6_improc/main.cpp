4afaff9c6e3cf329d06ee14b7a66cb6ce315c205
win7_64_en <1281261856@qq.com>
2017-04-26 21:32:12 +0800

    add chapter 6

---
+++ 6_improc/main.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+#pragma warning(push)
+#pragma warning(disable:4819)
+#include <opencv2/opencv.hpp>
+#pragma warning(pop)
+
+#include <functional>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+using namespace cv;
+
+
+
+int main()
+{
+
+}
