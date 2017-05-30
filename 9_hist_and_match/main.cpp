b7214e0030e660fb06f79701dfe46132e22f101b
win10_i5-4590 <1281261856@qq.com>
2017-05-31 00:05:14 +0800

    update chapter 9

--- 9_hist_and_match/main.cpp
+++ 9_hist_and_match/main.cpp
@@ -94,9 +94,133 @@ void test(const char*img) {
 }
 }
 
+
+// 9.2.5
+namespace rgb_histogram {
+void test(const char* img)
+{
+	Mat src = imread(img);
+	imshow("origin", src);
+
+	int bins = 256;
+	int hist_size[] = { bins };
+	float range[] = { 0,256 };
+	const float* ranges[] = { range };
+	Mat red, green, blue;
+	int channels_r[] = { 0 };
+
+	calcHist(&src, 1, channels_r, Mat(), red, 1, hist_size, ranges, true, false);
+
+	int channels_g[] = { 1 };
+	calcHist(&src, 1, channels_g, Mat(), green, 1, hist_size, ranges, true, false);
+
+	int channels_b[] = { 2 };
+	calcHist(&src, 1, channels_b, Mat(), blue, 1, hist_size, ranges, true, false);
+
+	double red_max, green_max, blue_max;
+	minMaxLoc(red, 0, &red_max);
+	minMaxLoc(green, 0, &green_max);
+	minMaxLoc(blue, 0, &blue_max);
+
+	int scale = 1;
+	int hist_height = 256;
+	Mat hist = Mat::zeros(hist_height, bins * 3, CV_8UC3);
+
+	for (int i = 0; i < bins; i++) {
+		auto bin_red = red.at<float>(i);
+		auto bin_green = green.at<float>(i);
+		auto bin_blue = blue.at<float>(i);
+
+		int intensity_red = cvRound(bin_red * hist_height / red_max);
+		int intensity_green = cvRound(bin_green * hist_height / green_max);
+		int intensity_blue = cvRound(bin_blue * hist_height / blue_max);
+
+		rectangle(hist, Point(i*scale, hist_height - 1), Point((i + 1)*scale - 1, hist_height - intensity_red), Scalar(255, 0, 0));
+		rectangle(hist, Point((i + bins)*scale, hist_height - 1), Point((i + bins + 1)*scale - 1, hist_height - intensity_green), Scalar(0, 255, 0));
+		rectangle(hist, Point((i + 2 * bins)*scale, hist_height - 1), Point((i + 2 * bins + 1)*scale - 1, hist_height - intensity_blue), Scalar(0, 0, 255));
+	}
+
+	imshow("rgb histogram", hist);
+	waitKey();
+}
+}
+
+
+// 9.3.2
+namespace hist_comparision {
+void test() 
+{
+	Mat src_base, src_test1, src_test2;
+	Mat hsv_base, hsv_test1, hsv_test2;
+	Mat hsv_halfdown;
+
+	src_base = imread("base.jpg");
+	src_test1 = imread("test1.jpg");
+	src_test2 = imread("test2.jpg");
+
+	imshow("origin", src_base);
+	imshow("test1", src_test1);
+	imshow("test2", src_test2);
+
+	cvtColor(src_base, hsv_base, COLOR_BGR2HSV);
+	cvtColor(src_test1, hsv_test1, COLOR_BGR2HSV);
+	cvtColor(src_test2, hsv_test2, COLOR_BGR2HSV);
+
+	hsv_halfdown = hsv_base(Range(hsv_base.rows / 2, hsv_base.rows - 1), Range(0, hsv_base.cols - 1));
+
+	int hbins = 50; int sbins = 60;
+	int hist_size[] = { hbins, sbins };
+	float hranges[] = { 0,256 };
+	float sranges[] = { 0,180 };
+	const float* ranges[] = { hranges, sranges };
+	int channels[] = { 0,1 };
+
+	Mat base_hist, halfdown_hist, test1_hist, test2_hist;
+
+	calcHist(&hsv_base, 1, channels, Mat(), base_hist, 2, hist_size, ranges, true, false);
+	normalize(base_hist, base_hist, 0, 1, NORM_MINMAX, -1, noArray());
+
+	calcHist(&hsv_halfdown, 1, channels, noArray(), halfdown_hist, 2, hist_size, ranges, true, false);
+	normalize(halfdown_hist, halfdown_hist, 0, 1, NORM_MINMAX, -1, noArray());
+
+	calcHist(&hsv_test1, 1, channels, noArray(), test1_hist, 2, hist_size, ranges, true, false);
+	normalize(test1_hist, test1_hist, 0, 1, NORM_MINMAX, -1, noArray());
+
+	calcHist(&hsv_test2, 1, channels, noArray(), test2_hist, 2, hist_size, ranges, true, false);
+	normalize(test2_hist, test2_hist, 0, 1, NORM_MINMAX);
+
+	const std::pair<int, const char*> compare_methods[] = { 
+		{CV_COMP_CORREL, "CV_COMP_CORREL"},
+		{CV_COMP_CHISQR, "CV_COMP_CHISQR"},
+		{CV_COMP_BHATTACHARYYA, "CV_COMP_BHATTACHARYYA"},
+		{CV_COMP_CHISQR_ALT, "CV_COMP_CHISQR_ALT "} 
+	};
+
+	for (auto compare_method : compare_methods) {
+		double base_base = compareHist(base_hist, base_hist, compare_method.first);
+		double base_half = compareHist(base_hist, halfdown_hist, compare_method.first);
+		double base_test1 = compareHist(base_hist, test1_hist, compare_method.first);
+		double base_test2 = compareHist(base_hist, test2_hist, compare_method.first);
+
+		cout << "method " << compare_method.second << ":" << endl
+			<< "\tbase-base: " << base_base << endl
+			<< "\tbase-half: " << base_half << endl
+			<< "\tbase-test1: " << base_test1 << endl
+			<< "\tbase-test2: " << base_test2 << endl;
+	}
+
+	waitKey();
+}
+}
+
+
 int main()
 {
 	//hue_saturation_histogram::test("hs_histogram.jpg");
 
-	one_dimension_histogram::test("one_dimension_histogram.jpg");
+	//one_dimension_histogram::test("one_dimension_histogram.jpg");
+
+	//rgb_histogram::test("rgb_histogram.jpg");
+
+	hist_comparision::test();
 }
