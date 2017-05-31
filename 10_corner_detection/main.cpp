ecfbd89ee7cf986fd199417b6b5444542bdb579e
win10_i5-4590 <1281261856@qq.com>
2017-05-31 23:17:49 +0800

    add subpix

--- 10_corner_detection/main.cpp
+++ 10_corner_detection/main.cpp
@@ -68,7 +68,114 @@ void test(const char* img)
 }
 
 
+// 10.2.3
+namespace shi_tomasi {
+auto win = "Shi-Tomasi corner detection";
+
+Mat src, gray;
+int corner = 33;
+int max_corner = 500;
+mt19937 rng;
+uniform_int_distribution<int> dist(0, 255);
+
+void on_trackbar(int = 0, void* = nullptr)
+{
+	corner = max(corner, 1);
+
+	vector<Point2f> corners;
+	double qualiti_level = 0.01;
+	double min_distance = 10;
+	int block_size = 3;
+	double k = 0.04;
+	Mat copy = src.clone();
+
+	goodFeaturesToTrack(gray, corners, corner, qualiti_level, min_distance, noArray(), block_size, false, k);
+
+	cout << "detected corners: " << corners.size() << endl;
+
+	int r = 4;
+	for (size_t i = 0; i < corners.size(); i++) {
+		circle(copy, corners[i], r, Scalar(dist(rng), dist(rng), dist(rng)), FILLED, LINE_8);
+	}
+
+	imshow(win, copy);
+}
+
+void test(const char* img)
+{
+	src = imread(img);
+	cvtColor(src, gray, COLOR_BGR2GRAY);
+
+	namedWindow(win);
+	createTrackbar("max corner", win, &corner, max_corner, on_trackbar);
+	on_trackbar();
+	waitKey();
+}
+
+}
+
+
+// 10.3.3
+namespace subpix {
+auto win = "sub-pix corner detection";
+
+Mat src, gray;
+int corner = 33;
+int max_corner = 500;
+mt19937 rng;
+uniform_int_distribution<int> dist(0, 255);
+
+void on_trackbar(int = 0, void* = nullptr)
+{
+	corner = max(corner, 1);
+
+	vector<Point2f> corners;
+	double qualiti_level = 0.01;
+	double min_distance = 10;
+	int block_size = 3;
+	double k = 0.04;
+	Mat copy = src.clone();
+
+	goodFeaturesToTrack(gray, corners, corner, qualiti_level, min_distance, noArray(), block_size, false, k);
+
+	cout << "detected corners: " << corners.size() << endl;
+
+	int r = 4;
+	for (size_t i = 0; i < corners.size(); i++) {
+		circle(copy, corners[i], r, Scalar(dist(rng), dist(rng), dist(rng)), FILLED, LINE_8);
+	}
+
+	imshow(win, copy);
+
+	Size winsize = Size(5, 5);
+	Size zerozone = Size(-1, -1);
+	TermCriteria criteria(TermCriteria::EPS + CV_TERMCRIT_ITER, 40, 0.001);
+
+	cornerSubPix(gray, corners, winsize, zerozone, criteria);
+
+	for (size_t i = 0; i < corners.size(); i++) {
+		cout << "\tprecision coordinate[" << i << "](" << corners[i].x << ", " << corners[i].y << ")" << endl;
+	}
+}
+
+void test(const char* img)
+{
+	src = imread(img);
+	cvtColor(src, gray, COLOR_BGR2GRAY);
+
+	namedWindow(win);
+	createTrackbar("max corner", win, &corner, max_corner, on_trackbar);
+	on_trackbar();
+	waitKey();
+}
+}
+
+
 int main()
 {
-	harris::test("harris.jpg");
+	//harris::test("harris.jpg");
+
+	//shi_tomasi::test("shi-tomasi.jpg");
+
+	subpix::test("subpix.jpg");
 }
