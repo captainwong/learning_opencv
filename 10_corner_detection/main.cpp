282e9d45b5fdff26e80297fa0ea99a72acf11d7d
win10_i5-4590 <1281261856@qq.com>
2017-05-31 22:56:29 +0800

    add harris

--- 10_corner_detection/main.cpp
+++ 10_corner_detection/main.cpp
@@ -16,10 +16,59 @@ using namespace std;
 using namespace cv;
 
 
+// 10.1.5
+namespace harris {
 
+auto win1 = "blend";
+auto win2 = "scale";
+
+Mat src, src2, gray;
+int thresh = 30;
+int max_thresh = 175;
+
+void on_trackbar(int = 0, void* = nullptr)
+{
+	Mat dst, norm, scale;
+	dst = Mat::zeros(src.size(), CV_32FC1);
+	src2 = src.clone();
+
+	cornerHarris(gray, dst, 2, 3, 0.04, BORDER_DEFAULT);
+	imshow("dst", dst);
+	normalize(dst, norm, 0, 255, NORM_MINMAX, CV_32FC1, noArray());
+	imshow("norm", norm);
+	convertScaleAbs(norm, scale);
+	imshow("scale before circle", scale);
+	for (int row = 0; row < norm.rows; row++) {
+		for (int col = 0; col < norm.cols; col++) {
+			if (cvRound(norm.at<float>(row, col)) > thresh + 80) {
+				circle(src2, Point(col, row), 5, Scalar(10, 10, 255), 2, LINE_8);
+				circle(scale, Point(col, row), 5, Scalar(0, 10, 255), 2, LINE_8);
+			}
+		}
+	}
+
+	imshow(win1, src2);
+	imshow(win2, scale);
+}
+
+void test(const char* img)
+{
+	src = imread(img);
+	src2 = src.clone();
+	imshow("origin", src);
+
+	cvtColor(src, gray, COLOR_BGR2GRAY);
+
+	namedWindow(win1);
+	createTrackbar("thresh", win1, &thresh, max_thresh, on_trackbar);
+	on_trackbar();
+	waitKey();
+}
+
+}
 
 
 int main()
 {
-
+	harris::test("harris.jpg");
 }
